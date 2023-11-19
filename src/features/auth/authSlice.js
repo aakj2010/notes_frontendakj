d705a9566e4d3adfcefd4d4c378752c93e6f5a52
aakj2010 <aakj2010@gmail.com>
2023-11-20 01:07:51 +0530

    removed unnecessary var

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -41,7 +41,7 @@ export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
   }
 })
 
-export const logout = createAsyncThunk(API_URL + 'auth/logout', async () => {
+export const logout = createAsyncThunk('auth/logout', async () => {
   await authService.logout()
 })
 
